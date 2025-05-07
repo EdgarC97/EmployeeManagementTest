284b2759182902b71d12d1bc2c135af531bb863e
EdgarC97 <ecardona742@gmail.com>
2025-05-07 17:11:33 -0500

    feat: create models

---
+++ EmployeeManagement.Web/Models/EmployeeViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace EmployeeManagement.Web.Models
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string FullName => $"{FirstName} {LastName}";
+        public string Address { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+        public string IdentificationNumber { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+    }
+}
---
+++ EmployeeManagement.Web/Models/UpdateEmployeeViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Web.Models
+{
+    public class UpdateEmployeeViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Phone Number is required")]
+        [Display(Name = "Phone Number")]
+        [Phone(ErrorMessage = "Invalid phone number format")]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Date of Birth is required")]
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Identification Number is required")]
+        [Display(Name = "Identification Number")]
+        public string IdentificationNumber { get; set; } = string.Empty;
+    }
+}
