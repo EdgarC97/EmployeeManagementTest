f11bd3dead506a03226836fb9cbb1aa15a090065
EdgarC97 <ecardona742@gmail.com>
2025-05-07 18:44:45 -0500

    feat: resolve exercises 3 and 4

---
+++ Exercise_3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Punto3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImprimirNumeros(0);
+        }
+
+        static void ImprimirNumeros(int numero)
+        {
+            if (numero > 100) return;
+
+            Console.WriteLine(numero);
+            ImprimirNumeros(numero + 3);
+        }
+    }
+}
---
+++ Exercise_4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Punto4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] entradas = { "hat", "abc", "Zu6" };
+            int contador = 1;
+            foreach (var palabra in entradas)
+            {
+                var permutaciones = ObtenerPermutaciones(palabra);
+                Console.WriteLine($"{contador} {string.Join(",", permutaciones)}");
+                contador++;
+            }
+        }
+
+        static List<string> ObtenerPermutaciones(string texto)
+        {
+            var resultado = new List<string>();
+            Permutar("", texto, resultado);
+            return resultado;
+        }
+
+        static void Permutar(string prefijo, string resto, List<string> resultado)
+        {
+            if (resto.Length == 0)
+            {
+                resultado.Add(prefijo);
+            }
+            else
+            {
+                for (int i = 0; i < resto.Length; i++)
+                {
+                    string nuevoPrefijo = prefijo + resto[i];
+                    string nuevoResto = resto.Substring(0, i) + resto.Substring(i + 1);
+                    Permutar(nuevoPrefijo, nuevoResto, resultado);
+                }
+            }
+        }
+    }
+}
